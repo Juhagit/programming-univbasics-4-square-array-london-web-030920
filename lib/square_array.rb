d19c8ba020fb0871893300873c87bcc0e81ae9e5
Juha Penttilä <juha.ea.penttila@gmail.com>
2020-02-23 04:28:35 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,13 +1,12 @@
 def square_array(array)
 
-counter = 0
-  new_array = []
-  while counter < array.length do
-    array[counter]**2
-    new_array.push(array[counter]**2)
-    counter += 1
-  end 
-    return new_array
+new_array = []
+ while counter < array.length do
+   new_array.push (array[counter]**2)
+   counter +=1 
+  
+ end
+ return new_array
 
 end
 
